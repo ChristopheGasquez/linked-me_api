56af2fd1b8dec3dd0e95916473d92af7c2a280a1
gasquez.christophe <c.gasquez.dl@gmail.com>
2026-02-20 23:28:54 +0100

    Add TasksController + cleanup expired tokens + limit refresh tokens to 10 per user

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminRolesModule } from './roles/roles.module.js';
 import { AdminUsersModule } from './users/users.module.js';
-import { MaintenanceModule } from './maintenance/maintenance.module.js';
 
 @Module({
-  imports: [AdminRolesModule, AdminUsersModule, MaintenanceModule],
+  imports: [AdminRolesModule, AdminUsersModule],
 })
 export class AdminModule {}
